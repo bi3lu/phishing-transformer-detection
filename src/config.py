87b153088210203f6f3cdc6540fcda3626391063
Jakub Bielecki <jakub.bielecki@axabee.com>
2026-02-23 14:48:07 +0100

    delete data (gitignore)

--- src/config.py
+++ src/config.py
@@ -3,9 +3,15 @@ from pathlib import Path
 # Paths:
 BASE_DIR = Path(__file__).resolve().parents[1]
 DATA_DIR = BASE_DIR / "data"
-RAW_DATA_DIR = DATA_DIR / "raw"
-PROCESSED_DATA_DIR = DATA_DIR / "processed"
-SPLIT_DATA_DIR = DATA_DIR / "split"
+RAW_DATA_DIR = (
+    DATA_DIR / "raw"
+)  # TODO: Think about store data in separate place, like cloud or db
+PROCESSED_DATA_DIR = (
+    DATA_DIR / "processed"
+)  # TODO: Think about store data in separate place, like cloud or db
+SPLIT_DATA_DIR = (
+    DATA_DIR / "split"
+)  # TODO: Think about store data in separate place, like cloud or db
 
 # Constants:
 RANDOM_STATE = 42
